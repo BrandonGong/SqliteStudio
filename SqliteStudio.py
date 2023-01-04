876fbc6a669c221fa2b9ebe22a8fc4d95e59aa7f
Brandon <BrandonGong@users.noreply.github.com>
2023-01-03 21:05:28 -0700

    Added Results view

--- SqliteStudio.py
+++ SqliteStudio.py
@@ -5,9 +5,13 @@ from kivy.app import App
 from kivy.factory import Factory
 
 from kivy.properties import StringProperty, ObjectProperty
+from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.treeview import TreeView, TreeViewLabel
 from kivy.uix.popup import Popup
+from kivy.core.window import Window
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 import sqlite3
 
@@ -30,12 +34,12 @@ class ObjectExplorer(BoxLayout):
             
         self.clear_widgets()
         tv = TreeView(root_options={'text':self.database_name})
-
         # Get list of tables
         for table in self.get_tables():
             table_node = tv.add_node(TreeViewLabel(text=table))
+            col_node = tv.add_node(TreeViewLabel(text="Columns"),table_node)
             for column in self.get_columns(table):
-                tv.add_node(TreeViewLabel(text=column),table_node)
+                tv.add_node(TreeViewLabel(text=column),col_node)
 
         self.add_widget(tv)
 
@@ -54,14 +58,36 @@ class ObjectExplorer(BoxLayout):
         except Exception as ex:
             print(ex)
             return []
-
-class Results(BoxLayout):
+class TableHeader(Label):
     pass
+class TableRow(TextInput):
+    pass
+class Results(BoxLayout):
+    grid = ObjectProperty(None)
 
-class MainWindow(BoxLayout):
+    def populate_results(self,results):
+        columns = results.description
+        data = results.fetchall()
+        self.grid.cols = len(results.description)
+        self.grid.rows = len(data) + 1
+        self.grid.clear_widgets()
+        for col in columns:
+            self.grid.add_widget(TableHeader(text=col[0]))
+        
+        for row in data:
+            for value in row:
+                self.grid.add_widget(TableRow(text= value and str(value) or 'Null'))
+
+class MainWindow(Widget):
     sql_text = StringProperty(None) # SQL text
     object_explorer = ObjectProperty(None) # Object explorer widget
+    results = ObjectProperty(None)
     connection = ObjectProperty(None) # Sqlite Connection
+
+    def __init__(self, **kwargs):
+        super(MainWindow, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self, 'text')
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
 
     def dismiss_popup(self):
         self._popup.dismiss()
@@ -71,7 +97,6 @@ class MainWindow(BoxLayout):
         self._popup = Popup(title="Load file", content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
-
 
     def load_database(self,file):
         if self.connection is not None:
@@ -90,18 +115,24 @@ class MainWindow(BoxLayout):
 
         self.dismiss_popup()
 
-    
     def run_query(self): 
         if not self.connection:
             return
         try:
             results = self.connection.execute(self.sql_text)
-            print(results.description)
-            print(results.fetchall())
+            self.results.populate_results(results)
             self.connection.commit()
         except Exception as ex:
             self.connection.rollback()
-            print(ex)        
+            print(ex)
+
+    def _keyboard_closed(self):
+         pass
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'f5':
+            self.run_query()
+        return True
 
 class SqliteStudioApp(App):
     def build(self):
